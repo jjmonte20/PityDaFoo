2cc5294893669a229a8191154b6dd73ea30e28d3
jjmonte20 <jjmonte20@gmail.com>
2018-11-06 13:17:10 -0600

    added script to grab input values to add to database, and push those values to a list, and console.log

--- requesthandler.js
+++ requesthandler.js
@@ -7,4 +7,50 @@ var config = {
     messagingSenderId: "241241817268"
   };
   firebase.initializeApp(config);
+
+  //variable to reference the database
+  var database = firebase.database();
+
+  // initial variables
+  var name = "";
+  var role = "";
+  var start = "";
+  var mWork = "";
+  var mRate = "";
+  var tBill = "";
+
+
+  var list = [];
+  
+
+  $("#submitButton").on("click", function(event){
+
+    //prevent the page from refreshing
+    event.preventDrefault();
+
+    // grab the inputs from the entry fields
+    name = $("#employeeNameInput").val().trim();
+    role = $("#employeeRoleInput").val().trim();
+    start = $("#employeeStartDateInput").val().trim();
+    mRate = $("#employeeMonthlyRateInput").val().trim();
+    
+    //want to make a row that adds information in the input fields
+    databade.ref().push({
+      name: name,
+      role: role,
+      start: start,
+      mWork: mWork,
+      mRate: mRate,
+      tBill: tBill
+    });
+
+    
+  });
+
+  database.ref().on("child_added", function(snapshot){
+    console.log(snapshot.val());
+  }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+
   
