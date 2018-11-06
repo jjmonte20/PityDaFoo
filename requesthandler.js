55faddb33068352afc47cb810a97aba03555f4f9
jjmonte20 <jjmonte20@gmail.com>
2018-11-06 11:51:40 -0600

    added firebase scripts

---
+++ requesthandler.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDvR6nqSedj7Vxi5o_yMggbZhG19JcHnZA",
+    authDomain: "employeeindex.firebaseapp.com",
+    databaseURL: "https://employeeindex.firebaseio.com",
+    projectId: "employeeindex",
+    storageBucket: "employeeindex.appspot.com",
+    messagingSenderId: "241241817268"
+  };
+  firebase.initializeApp(config);
+  
